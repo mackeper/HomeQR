36855ed52b18f0e916dbf559c8bd26ec79f0e5ca
Marcus Östling <mpt.ostling@gmail.com>
2023-10-01 01:40:54 +0200

    Setup the blazor app and QR generation

---
+++ src/HomeQR/Data/QrCode.cs
@@ -0,0 +1,30 @@
+﻿using QRCoder;
+
+namespace HomeQR.Data;
+
+public class QrCode
+{
+    public int Id { get; }
+    public string Name { get; }
+
+    public byte[] Code { get; }
+
+    public string Href { get; }
+
+    public QrCode(int id, string name, string href)
+    {
+        Id = id;
+        Name = name;
+        Code = GenerateQrCode(href);
+        Href = href;
+    }
+
+    private static byte[] GenerateQrCode(string data)
+    {
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+        var qrCode = new PngByteQRCode(qrCodeData);
+        return qrCode.GetGraphic(4, new byte[] { 0, 0, 0, 255 }, new byte[] { 255, 255, 255, 0 }, false);
+    }
+
+}
---
+++ src/HomeQR/Data/QrCodesService.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeQR.Data;
+
+public class QrCodesService
+{
+    public Task<QrCode[]> GetQrCodes()
+        => Task.FromResult(new[]
+        {
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+            new QrCode(1, "Tvättmaskin", "https//realmoneycompany.com"),
+        });
+}
